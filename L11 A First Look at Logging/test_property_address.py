e8ec579451fbb7dcaf707d3d80a469d89a48f59c
Pavel Dvorkin <dpavel20@gmail.com>
2014-07-24 00:00:31 -0400

    Update test_property_address.py

--- L11 A First Look at Logging/test_property_address.py
+++ L11 A First Look at Logging/test_property_address.py
@@ -6,7 +6,7 @@ class TestAddresses(unittest.TestCase):
     def setUp(self): 
         self.home = Address( name='Steve Holden', street_address='1972 Flying Circus', city='Arlington', state='VA', zip_code='12345' )
        
-    def test_name(self): 
+    def test_name(self):
         self.assertEqual(self.home.name, 'Steve Holden') 
         self.assertRaises(AttributeError, setattr, self.home, 'name', 'Daniel Greenfeld')  
            
@@ -18,9 +18,10 @@ class TestAddresses(unittest.TestCase):
         
     def test_zip_code(self): 
         self.assertEqual(self.home.zip_code, '12345') 
-        self.assertRaises(ZipCodeError, setattr, self.home, 'zip_code', '123456')   
+        self.assertRaises(ZipCodeError, setattr, self.home, 'zip_code', '123456')
         self.home.zip_code = '54321' 
         self.assertEqual(self.home.zip_code, '54321') 
        
-if __name__ == "__main__": 
-    unittest.main() 
+if __name__ == "__main__":
+    start_logging(level="info")
+    unittest.main()
